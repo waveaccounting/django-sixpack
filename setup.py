db8dbff2b0e4daccfa80b070a15c72281342d5bb
Daniel Langer <daniel@langer.me>
2014-12-17 16:48:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Framework :: Django',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
     ),
